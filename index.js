e5ee038b8eb8e02765d905c3a33a8c1d81405350
Raju Saha <59817023+RajuSaha1693@users.noreply.github.com>
2021-05-22 10:53:18 +0530

    Update index.js

--- index.js
+++ index.js
@@ -20,7 +20,7 @@ app.get('/',(req,res)=>{
 const CONNECTION_URL='mongodb://rootuser:rootuser@rajusaha1693-mongo-production/memories'
 const PORT=process.env.PORT || 5000
 
-mongoose.connect(process.env.CONNECTION_URL,{useNewUrlParser:true,useUnifiedTopology:true})
+mongoose.connect(CONNECTION_URL,{useNewUrlParser:true,useUnifiedTopology:true})
     .then(()=>app.listen(PORT,()=>console.log(`Server running on port ${PORT}`)))
     .catch((error)=>console.log(error.message))
 
