dd74dc6bb5aa3cd7a0ac2bb88d9e1bf0a9a159bf
Raju Saha <59817023+RajuSaha1693@users.noreply.github.com>
2021-05-22 10:17:57 +0530

    Add files via upload

---
+++ models/user.js
@@ -0,0 +1,12 @@
+import mongoose from 'mongoose'
+
+const userSchema=mongoose.Schema({
+    name:{type:String,required:true},
+    email:{type:String,require:true},
+    password:{type:String,required:true},
+    id:{type:String}
+  
+});
+
+export default mongoose.model('User',userSchema);
+
